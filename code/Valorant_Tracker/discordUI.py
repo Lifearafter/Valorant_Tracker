972d5cfacac30c98ede7ce27a3b6932ae8f39d1f
Muhammad Zaid <mxz190024@utdallas.edu>
2022-06-30 03:45:36 +0500

    refactor: Seperated Code and Tests

--- code/Valorant_Tracker/discordUI.py
+++ code/Valorant_Tracker/discordUI.py
@@ -1,6 +1,6 @@
 import os
 from discord.ext import commands
-from matchhistory import compMatchHistory
+from code.Valorant_Tracker.matchhistory import compMatchHistory
 from dotenv import load_dotenv
 
 bot = commands.Bot(command_prefix='!')
